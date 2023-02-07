cd531973bcd4113712660a097694f4f83ea8a5bc
lbj0412 <60974852+lbj0412@users.noreply.github.com>
2023-02-07 09:08:26 +0000

    terraform_basic

---
+++ 07.cloudsql/cloudsql.tf
@@ -0,0 +1,39 @@
+resource "google_compute_global_address" "private_ip_address" {
+  provider = google-beta
+
+  name          = "private-ip-address"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = 16
+  network       = google_compute_network.vpc_network.id
+}
+
+resource "google_service_networking_connection" "private_vpc_connection" {
+  provider = google-beta
+
+  network                 = google_compute_network.vpc_network.id
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
+}
+
+resource "random_id" "db_name_suffix" {
+  byte_length = 4
+}
+
+resource "google_sql_database_instance" "instance" {
+  provider = google-beta
+
+  name             = "private-instance-${random_id.db_name_suffix.hex}"
+  region           = "us-central1"
+  database_version = "MYSQL_5_7"
+
+  depends_on = [google_service_networking_connection.private_vpc_connection]
+  
+  settings {
+    tier = "db-n1-standard-2"
+    ip_configuration {
+      ipv4_enabled    = false
+      private_network = google_compute_network.vpc_network.id
+    }
+  }
+}
